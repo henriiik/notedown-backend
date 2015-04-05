6746277f009b0b2c3967b71ab4645125f973285f
Henrik Rosén <henriiik@gmail.com>
2015-04-05 14:50:23 +0200

    Update wsgi.py

--- notedown/notedown/wsgi.py
+++ notedown/notedown/wsgi.py
@@ -8,6 +8,20 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'notedown.settings'
+
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'notedown'))
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/venv/'
+
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv,
+                                              'lib/python3.3/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except:
+    pass
 
 from django.core.wsgi import get_wsgi_application
 
