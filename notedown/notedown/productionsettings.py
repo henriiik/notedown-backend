5eb82700f1a1f945341090c57c68ba6b3e2a615e
Henrik Rosén <henriiik@gmail.com>
2015-04-06 15:33:15 +0200

    update production settings to reflect new openshift server name

--- notedown/notedown/productionsettings.py
+++ notedown/notedown/productionsettings.py
@@ -2,13 +2,13 @@ from notedown.settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-    'python-enhenrik.rhcloud.com'
+    'backend-enhenrik.rhcloud.com'
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'python',
+        'NAME': 'backend',
         'USER': os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
         'PASSWORD': os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],
         'HOST': os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
